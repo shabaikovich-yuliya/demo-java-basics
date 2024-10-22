329ae0890a7d358966f03d6a9f6cf52dbef75b72
Yuliya <yuliya.01707@gmail.com>
2024-10-22 21:16:12 +0200

    add Test2 - filling height and weight

--- src/test/java/biz/svyatoslav/CalculatorTest.java
+++ src/test/java/biz/svyatoslav/CalculatorTest.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.nio.file.WatchEvent;
-
 public class CalculatorTest {
     @Test
     public void test1(){
@@ -21,5 +19,29 @@ public class CalculatorTest {
         By by = By.xpath(xpath);
         WebElement webElement = webDriver.findElement(by);
         webElement.click();
+    }
+    @Test
+    public void test2(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://svyatoslav.biz/testlab/wt/index.php");
+        String inputNameXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[2]/td[2]/input";
+        By inputNameBy = By.xpath(inputNameXpath);
+        WebElement inputNameWebElement = driver.findElement(inputNameBy);
+        inputNameWebElement.sendKeys("John");
+
+        String inputHeightXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[3]/td[2]/input";
+        By inputHeightBy = By.xpath(inputHeightXpath);
+        WebElement inputHeightWebElement = driver.findElement(inputHeightBy);
+        inputHeightWebElement.sendKeys("180");
+
+        String inputWeightXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[4]/td[2]/input";
+        By inputWeightBy = By.xpath(inputWeightXpath);
+        WebElement inputWeightWebElement = driver.findElement(inputWeightBy);
+        inputWeightWebElement.sendKeys("70");
+
+        String buttonCalculateXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
+        By buttonCalculateBy = By.xpath(buttonCalculateXpath);
+        WebElement buttonCalculateWebElement = driver.findElement(buttonCalculateBy);
+        buttonCalculateWebElement.click();
     }
 }
