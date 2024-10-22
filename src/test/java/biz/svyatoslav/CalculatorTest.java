e5977dc8214bfa02fbf9cfef530a8d02ca892f8e
Yuliya <yuliya.01707@gmail.com>
2024-10-22 20:54:48 +0200

    add filling Name

--- src/test/java/biz/svyatoslav/CalculatorTest.java
+++ src/test/java/biz/svyatoslav/CalculatorTest.java
@@ -6,11 +6,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.nio.file.WatchEvent;
+
 public class CalculatorTest {
     @Test
     public void test1(){
         WebDriver webDriver = new ChromeDriver();
         webDriver.get("https://svyatoslav.biz/testlab/wt/index.php");
+        String name = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[2]/td[2]/input";
+        By nameA = By.xpath(name);
+        WebElement filledName = webDriver.findElement(nameA);
+        filledName.sendKeys("John");
         String xpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
         By by = By.xpath(xpath);
         WebElement webElement = webDriver.findElement(by);
