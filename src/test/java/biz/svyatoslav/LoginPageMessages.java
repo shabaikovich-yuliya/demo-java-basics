7bc0ce56435e98f29f87934a2cb13f98e5beb564
Yuliya <yuliya.01707@gmail.com>
2024-10-27 17:43:40 +0100

    add constants of messages for CalculatorTest

---
+++ src/test/java/biz/svyatoslav/LoginPageMessages.java
@@ -0,0 +1,22 @@
+package biz.svyatoslav;
+
+public class LoginPageMessages {
+    public static final String INVALID_HEIGHT_AND_INVALID_WEIGHT = "Рост должен быть в диапазоне 50-300 см.\n" +
+            "Вес должен быть в диапазоне 3-500 кг.";
+    public static final String INVALID_HEIGHT = "Рост должен быть в диапазоне 50-300 см.";
+    public static final String INVALID_GENDER = "Не указан пол.";
+    public static final String INVALID_WEIGHT_AND_GENDER = "Вес должен быть в диапазоне 3-500 кг.\n" +
+            "Не указан пол.";
+    public static final String INVALID_NAME_AND_HEIGHT_AND_GENDER =  "Не указано имя.\n" +
+            "Рост должен быть в диапазоне 50-300 см.\n" +
+            "Не указан пол.";
+    public static final String INVALID_NAME_AND_WEIGH_AND_HEIGHT_AND_GENDER = "Не указано имя.\n" +
+            "Рост должен быть в диапазоне 50-300 см.\n" +
+            "Вес должен быть в диапазоне 3-500 кг.\n" +
+            "Не указан пол.";
+    public static final String RESULT_IDEAL = "Идеальная масса тела";
+    public static final String RESULT_MIDDLE_OVERWEIGHT = "Умеренный избыток массы тела";
+    public static final String RESULT_MAX_OVERWEIGHT = "Значительный избыток массы тела, тучность";
+    public static final String RESULT_UNDERWEIGHT = "Слишком малая масса тела";
+}
+
